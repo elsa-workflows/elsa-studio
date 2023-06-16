1138b60569d332002624a7fbc5a8651d9808a1a3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-06-16 11:20:19 +0200

    Implement resize and keyboard shortcuts

--- src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/create-graph.ts
+++ src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/create-graph.ts
@@ -2,6 +2,8 @@ import {Graph, Shape,} from '@antv/x6';
 import {Selection} from "@antv/x6-plugin-selection";
 import {Snapline} from "@antv/x6-plugin-snapline";
 import {Transform} from "@antv/x6-plugin-transform";
+import {Keyboard} from "@antv/x6-plugin-keyboard";
+import { Clipboard } from '@antv/x6-plugin-clipboard'
 import {v4 as uuid} from 'uuid';
 import {graphs} from "../internal/graphs";
 
@@ -92,19 +94,28 @@ export async function createGraph(containerId: string): Promise<string> {
             rubberNode: true,
             rubberband: true,
             movable: true,
-            showNodeSelectionBox: true
+            showNodeSelectionBox: false
         }),
     );
+
+    graph.use(
+        new Keyboard({
+            enabled: true
+        })
+    );
+
+    graph.use(
+        new Clipboard({
+            enabled: true,
+        }),
+    )
 
     // We could enable this, but then we also need to update the Blazor activity component to support resizing.
     graph.use(
         new Transform({
             resizing: {
-                enabled: false,
+                enabled: true,
 
-            },
-            rotating: {
-                enabled: false,
             }
         })
     );
@@ -133,7 +144,35 @@ export async function createGraph(containerId: string): Promise<string> {
             cell.removeTool("button-remove");
         }
     });
-    
+
+    // Delete node when pressing delete key on keyboard.
+    graph.bindKey('del', () => {
+        const cells = graph.getSelectedCells()
+        if (cells.length) {
+            graph.removeCells(cells)
+        }
+    });
+
+    // Copy the cells in the graph to the internal clipboard with Ctrl+C.
+    graph.bindKey(['ctrl+c', 'meta+c'], () => {
+        const cells = graph.getSelectedCells()
+        if (cells.length) {
+            graph.copy(cells)
+        }
+        return false
+    })
+
+    // Paste the cells in the clipboard onto the graph.
+    graph.bindKey(['ctrl+v', 'meta+v'], () => {
+        if (!graph.isClipboardEmpty()) {
+            const cells = graph.paste({ offset: 32 })
+            graph.cleanSelection()
+            graph.select(cells)
+        }
+        return false
+    })
+
+    // Register the graph.
     const graphId = containerId;
     graphs[graphId] = graph;
 
