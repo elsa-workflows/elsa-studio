b262be94ab00897ea035ef56df9520287879b5b2
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-06-26 21:00:03 +0200

    Remove unnecessary disposal of graph, causing many events to be emitted

--- src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/dispose-graph.ts
+++ src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/dispose-graph.ts
@@ -1,7 +1,5 @@
 import {graphBindings} from "./graph-bindings";
 
 export function disposeGraph(graphId) {
-    const binding = graphBindings[graphId];
-    binding.graph.dispose();
     delete graphBindings[graphId];
 }
