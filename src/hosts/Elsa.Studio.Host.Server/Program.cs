ba0304e40db5db77c5c032651cda7cd9624e1f7d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-06-26 20:58:47 +0200

    Increase SignalR allowed message size

--- src/hosts/Elsa.Studio.Host.Server/Program.cs
+++ src/hosts/Elsa.Studio.Host.Server/Program.cs
@@ -54,12 +54,21 @@ builder.Services.AddScoped<AuthenticationStateProvider, DefaultAuthenticationSta
 // Register the hosted services.
 builder.Services.AddHostedService<RunStartupTasksHostedService>();
 
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize to 512kb:
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
 // Build the application.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseResponseCompression();
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
