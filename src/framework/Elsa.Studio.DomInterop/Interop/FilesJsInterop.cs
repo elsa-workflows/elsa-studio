89b1db031049348012a844de3f4ad7c42b4bc7cd
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-08-23 09:47:40 +0200

    Fix dom interop module name mismatch

--- src/framework/Elsa.Studio.DomInterop/Interop/FilesJsInterop.cs
+++ src/framework/Elsa.Studio.DomInterop/Interop/FilesJsInterop.cs
@@ -3,14 +3,20 @@ using Microsoft.JSInterop;
 
 namespace Elsa.Studio.DomInterop.Interop;
 
+/// <summary>
+/// Provides JS interop methods for the files module.
+/// </summary>
 public class FilesJsInterop : JsInteropBase, IFiles
 {
+    /// <inheritdoc />
     public FilesJsInterop(IJSRuntime jsRuntime) : base(jsRuntime)
     {
     }
 
-    protected override string ModuleName => "download";
+    /// <inheritdoc />
+    protected override string ModuleName => "files";
 
+    /// <inheritdoc />
     public async Task DownloadFileFromStreamAsync(string fileName, Stream stream)
     {
         var streamRef = new DotNetStreamReference(stream);
