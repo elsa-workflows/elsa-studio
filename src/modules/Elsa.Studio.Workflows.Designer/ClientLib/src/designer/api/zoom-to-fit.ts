9afb295f1137c29beb75a8869a2e6e9535f12139
Mohamed Ali <antivirus.cs@gmail.com>
2023-10-13 16:23:04 +0300

    fix formatting

--- src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/zoom-to-fit.ts
+++ src/modules/Elsa.Studio.Workflows.Designer/ClientLib/src/designer/api/zoom-to-fit.ts
@@ -2,7 +2,7 @@ import {graphBindings} from "./graph-bindings";
 
 export function zoomToFit(graphId: string) {
     const {graph} = graphBindings[graphId];
-        graph.zoomToFit({
+    graph.zoomToFit({
         padding: 20,
         minScale: 0.5,
         maxScale: 3
