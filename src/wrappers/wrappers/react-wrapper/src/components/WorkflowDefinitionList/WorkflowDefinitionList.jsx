d777ea68ccde43dda111320f480fb99860474306
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-12-30 22:21:34 +0100

    Integrate useEffect in WorkflowDefinitionList component
    In this update, useEffect hook has been added to the WorkflowDefinitionList component. It's been added to handle an 'editWorkflowDefinition' function which is attached to the component's current DOM element. From now on, a callback will be triggered when a workflow definition is edited, which is passed as a prop from the parent component.

--- src/wrappers/wrappers/react-wrapper/src/components/WorkflowDefinitionList/WorkflowDefinitionList.jsx
+++ src/wrappers/wrappers/react-wrapper/src/components/WorkflowDefinitionList/WorkflowDefinitionList.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {v4 as uuid} from 'uuid';
 import {RemoteArgs} from "../../remote-args.js";
 
-
 /**
  * Primary UI component for user interaction
  */
-export const WorkflowDefinitionList = ({ remoteEndpoint, apiKey, ...props }) => {
+export const WorkflowDefinitionList = (props) => {
     const id = uuid();
+    const { remoteEndpoint, apiKey } = props;
+
+    useEffect(() => {
+        let element = document.getElementById(id);
+        element.editWorkflowDefinition = (definitionId) => {
+            props.onEditWorkflowDefinition(definitionId);
+        };
+    }, [id, props]); // Dependency array includes what the effect depends on to run.
+
     return React.createElement('elsa-workflow-definition-list', { ...props, id: id, "remote-endpoint": remoteEndpoint, "api-key": apiKey });
-};
+}
 
 WorkflowDefinitionList.propTypes = {
     /**
@@ -25,11 +33,14 @@ WorkflowDefinitionList.propTypes = {
      * The access token, if any.
      */
     accessToken: PropTypes.string,
+    /**
+     * The callback invoked when a workflow definition is edited.
+     */
+    onEditWorkflowDefinition: PropTypes.func,
 };
 
 WorkflowDefinitionList.defaultProps = {
     remoteEndpoint: RemoteArgs.remoteEndpoint,
     apiKey: RemoteArgs.apiKey,
-    accessToken: null,
-
+    accessToken: null
 };
