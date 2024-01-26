b295325ae59fdda6c3d856ad5552745d4cf9539f
Mohamed Ali <antivirus.cs@gmail.com>
2024-01-26 15:55:19 +0300

    update login logic to match other API calls

--- src/modules/Elsa.Studio.Login/Services/DefaultCredentialsValidator.cs
+++ src/modules/Elsa.Studio.Login/Services/DefaultCredentialsValidator.cs
@@ -4,6 +4,7 @@ using Elsa.Studio.Contracts;
 using Elsa.Studio.Login.Contracts;
 using Elsa.Studio.Login.Models;
 using Refit;
+using System.Net.Http;
 
 namespace Elsa.Studio.Login.Services;
 
@@ -13,20 +14,23 @@ namespace Elsa.Studio.Login.Services;
 public class DefaultCredentialsValidator : ICredentialsValidator
 {
     private readonly IRemoteBackendApiClientProvider _remoteBackendApiClientProvider;
+    private readonly IHttpClientFactory _httpClientFactory;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="DefaultCredentialsValidator"/> class.
     /// </summary>
-    public DefaultCredentialsValidator(IRemoteBackendApiClientProvider remoteBackendApiClientProvider)
+    public DefaultCredentialsValidator(IRemoteBackendApiClientProvider remoteBackendApiClientProvider, IHttpClientFactory httpClientFactory)
     {
         _remoteBackendApiClientProvider = remoteBackendApiClientProvider;
+        _httpClientFactory = httpClientFactory;
+
     }
 
     /// <inheritdoc />
     public async ValueTask<ValidateCredentialsResult> ValidateCredentialsAsync(string username, string password, CancellationToken cancellationToken = default)
     {
-        var serverUrl = _remoteBackendApiClientProvider.Url.ToString();
-        var api = RestService.For<ILoginApi>(serverUrl);
+        var api = await _remoteBackendApiClientProvider.GetApiAsync<ILoginApi>(cancellationToken);
+
         var request = new LoginRequest(username, password);
         var response = await api.LoginAsync(request, cancellationToken);
 
