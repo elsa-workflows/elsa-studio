3a433356560c5dd5c8cb679c24867563dd0b2ea3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-15 09:59:38 +0200

    Update icon + port provider for DownloadHttpFile activity (#226)
    * Update code comments and run configurations
    
    Improved readability and understanding of code by modifying and adding comments in the HttpStatusCodes.razor.cs and ActivityRegistryRefreshed.cs files. Also adjusted the EXE_PATH in the run configurations in Server.run.xml and Wasm.run.xml for more accurate path references.
    
    * Rename and enhance FlowSendHttpRequestPortProvider
    
    The class 'FlowSendHttpRequestPortProvider' has been renamed to 'FlowHttpRequestPortProvider' and enhanced to provide support also for the 'DownloadHttpFile' activity. The ServiceCollectionExtensions have been adjusted to reflect this name change.
    
    * Add DownloadHttpFile to DefaultActivityDisplaySettingsProvider
    
    A new activity called "Elsa.DownloadHttpFile" has been added to the DefaultActivityDisplaySettingsProvider. This activity is associated with HTTP colors and the CloudDownload icon, indicating its functionality for file downloading operations over HTTP.
    
    * Update package version in GitHub Actions workflow
    
    The GitHub Actions workflow file has been updated to reflect a new version for the packaged output. Instead of producing a package named with a tag of "3.2.0-PACKAGE_PREFIX.run_number", it will now generate a package with a name formed by "3.2.0-rc2.run_number".
    
    * Update branch trigger paths in packages.yml workflow
    
    This update adds new branch naming conventions to trigger actions in our packages workflow. It includes additions such as 'feat/*', 'bug/*', 'enh/*', 'perf/*', 'hotfix/*', and 'chore/*' to expand the workflow's flexibility and responsiveness to a wider range of branch names.

--- src/modules/Elsa.Studio.Workflows.Core/Domain/Notifications/ActivityRegistryRefreshed.cs
+++ src/modules/Elsa.Studio.Workflows.Core/Domain/Notifications/ActivityRegistryRefreshed.cs
@@ -3,7 +3,7 @@ using Elsa.Studio.Contracts;
 namespace Elsa.Studio.Workflows.Domain.Notifications;
 
 /// <summary>
-/// Represents a notification that is published when the activity registry is refreshed.
+/// Represents a notification published when the activity registry is refreshed.
 /// </summary>
 public class ActivityRegistryRefreshed : INotification
 {
