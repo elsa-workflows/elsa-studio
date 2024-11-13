258952eef9ff85fcc4c77be2351ed876d8f1cab5
Mohamed Ali <ali@ymacctv.com>
2024-11-13 12:26:01 +0300

    Fix build

--- src/modules/Elsa.Studio.Dashboard/Menu/DashboardMenu.cs
+++ src/modules/Elsa.Studio.Dashboard/Menu/DashboardMenu.cs
@@ -1,5 +1,6 @@
 using Elsa.Studio.Contracts;
 using Elsa.Studio.Models;
+using Elsa.Studio.Services;
 using Elsa.Studio.Workflows.Services;
 using Microsoft.AspNetCore.Components.Routing;
 using MudBlazor;
