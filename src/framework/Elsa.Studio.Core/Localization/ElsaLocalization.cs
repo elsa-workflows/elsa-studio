7e34ced733c11120e6971f88dc3112d40186f2fb
Mohamed Ali <ali@ymacctv.com>
2024-11-15 09:19:55 +0300

    refactoring

--- src/framework/Elsa.Studio.Core/Localization/ElsaLocalization.cs
+++ src/framework/Elsa.Studio.Core/Localization/ElsaLocalization.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.Extensions.Localization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Elsa.Studio.Resources;
 using System.Globalization;
 
