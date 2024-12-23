9d9ad94fbb93b3bf5b72784e0173d8b26a3d68d1
geffzhang <zhangshanyou@ai4c.cn>
2024-12-23 17:03:28 +0800

    feat: customize Elsa logo and the title
    https://github.com/elsa-workflows/elsa-studio/issues/52

---
+++ src/hosts/Elsa.Studio.Host.Server/StudioBrandingProvider.cs
@@ -0,0 +1,9 @@
+﻿using Elsa.Studio.Branding;
+
+namespace Elsa.Studio.Host.Server
+{
+    public class StudioBrandingProvider: DefaultBrandingProvider
+    {
+        public override string AppName => $"Elsa Studio"; 
+    }
+}
