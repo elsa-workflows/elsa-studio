f0a4c5e99fd713cceff18eda270ee46a1430c3c3
MariusVuscanNx <96233009+MariusVuscanNx@users.noreply.github.com>
2025-02-12 16:27:57 +0200

    Fixed incidents bugs (#415)
    * Fixed bug: Long incident title overflows
    
    * Fixed journal action on double click activity from sub workflow
    
    * Added null check for incidents activity linking

--- src/modules/Elsa.Studio.Workflows/Shared/Components/DiagramDesignerWrapper.razor.cs
+++ src/modules/Elsa.Studio.Workflows/Shared/Components/DiagramDesignerWrapper.razor.cs
@@ -83,10 +83,10 @@ public partial class DiagramDesignerWrapper
         var activities = containerActivity.GetActivities();
         var activityToSelect = activities.FirstOrDefault(x => x.GetNodeId() == nodeId);
 
-        await SelectActivityAsync(activityToSelect);
+        await SelectActivityAsync(activityToSelect, nodeId);
     }
 
-    private async Task SelectActivityAsync(JsonObject? activityToSelect)
+    private async Task SelectActivityAsync(JsonObject? activityToSelect, string? nodeId = null)
     {
         if (activityToSelect != null)
         {
@@ -94,8 +94,11 @@ public partial class DiagramDesignerWrapper
             return;
         }
 
+        if (nodeId == null)
+            return;
+        
         // Load the selected node path from the backend.
-        var pathSegmentsResponse = await WorkflowDefinitionService.GetPathSegmentsAsync(WorkflowDefinitionVersionId, activityToSelect!.GetNodeId());
+        var pathSegmentsResponse = await WorkflowDefinitionService.GetPathSegmentsAsync(WorkflowDefinitionVersionId, nodeId);
 
         if (pathSegmentsResponse == null)
             return;
