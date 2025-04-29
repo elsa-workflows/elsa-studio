bbdfa20ed4528062936fddd3cb8fd6363728ba01
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-04-29 15:03:03 +0200

    Update version to Elsa Studio 3.6
    Updated the application version in the HTML title and branding provider class to reflect the transition from version 3.5 to 3.6. This ensures consistent versioning across the application.

--- src/hosts/Elsa.Studio.Host.Server/StudioBrandingProvider.cs
+++ src/hosts/Elsa.Studio.Host.Server/StudioBrandingProvider.cs
@@ -5,6 +5,6 @@ namespace Elsa.Studio.Host.Server
     /// <inheritdoc />
     public class StudioBrandingProvider : DefaultBrandingProvider
     {
-        public override string AppName => "Elsa Studio 3.5";
+        public override string AppName => "Elsa Studio 3.6";
     }
 }
