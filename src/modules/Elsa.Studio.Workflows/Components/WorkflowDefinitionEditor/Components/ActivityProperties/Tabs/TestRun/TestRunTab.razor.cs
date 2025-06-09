1e46d4f2d370b5bbbda4d00821521f1e31549675
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-06-09 10:01:42 +0200

    Add activity testing API integration and activity test UI support
    Introduced `ITestsApi` and supporting models for activity testing in workflows. Updated the UI to support triggering and displaying activity test results, including handling outcomes, outputs, exceptions, and status. Additionally, updated dependency injection for the new API and adjusted the project references.

--- src/modules/Elsa.Studio.Workflows/Components/WorkflowDefinitionEditor/Components/ActivityProperties/Tabs/TestRun/TestRunTab.razor.cs
+++ src/modules/Elsa.Studio.Workflows/Components/WorkflowDefinitionEditor/Components/ActivityProperties/Tabs/TestRun/TestRunTab.razor.cs
@@ -1,6 +1,9 @@
-using System.Data;
 using System.Text.Json.Nodes;
+using Elsa.Api.Client.Extensions;
 using Elsa.Api.Client.Resources.ActivityDescriptors.Models;
+using Elsa.Api.Client.Resources.Tests;
+using Elsa.Api.Client.Resources.WorkflowDefinitions.Models;
+using Elsa.Studio.Contracts;
 using Elsa.Studio.Models;
 using Microsoft.AspNetCore.Components;
 
@@ -12,6 +15,11 @@ namespace Elsa.Studio.Workflows.Components.WorkflowDefinitionEditor.Components.A
 public partial class TestRunTab
 {
     /// <summary>
+    /// The workflow definition.
+    /// </summary>
+    [Parameter] public WorkflowDefinition WorkflowDefinition { get; set; } = null!;
+    
+    /// <summary>
     /// The activity descriptor.
     /// </summary>
     [Parameter] public ActivityDescriptor ActivityDescriptor { get; set; } = null!;
@@ -21,14 +29,27 @@ public partial class TestRunTab
     /// </summary>
     [Parameter] public JsonObject Activity { get; set; } = null!;
     
+    [Inject] private IBackendApiClientProvider BackendApiClientProvider { get; set; } = null!;
+    
     private DataPanelModel TestRunResults { get; } = new();
     private bool IsRunning { get; set; }
 
     private async Task OnTestRunClick()
     {
+        var testsApi = await BackendApiClientProvider.GetApiAsync<ITestsApi>();
+        var request = new TestActivityRequest
+        {
+            WorkflowDefinitionHandle = WorkflowDefinitionHandle.ByDefinitionVersionId(WorkflowDefinition.Id),
+            ActivityHandle = ActivityHandle.FromActivityNodeId(Activity.GetNodeId())
+        };
         IsRunning = true;
         StateHasChanged();
-        await Task.Delay(1000);
+        var response = await testsApi.TestActivityAsync(request);
+        TestRunResults.Clear();
+        TestRunResults.Add("Status", response.Status.ToString());
+        if (response.Outcomes != null) TestRunResults.Add("Outcomes", string.Join(", ", response.Outcomes));
+        if(response.Outputs != null) TestRunResults.Add("Outputs", response.Outputs.SerializeToNode().ToString());
+        if(response.Exception != null) TestRunResults.Add("Exception", response.Exception.SerializeToNode().ToString());
         IsRunning = false;
         StateHasChanged();
     }
