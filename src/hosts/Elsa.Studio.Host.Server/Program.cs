6e4c1dbaeda9e7e669fec8f1f369626305cb0d14
Matt <knibbsy10@live.com>
2025-06-29 14:42:49 +0100

    Comment out OAuth2 and use ElsaIdentity. Capitalize Tab text.

--- src/hosts/Elsa.Studio.Host.Server/Program.cs
+++ src/hosts/Elsa.Studio.Host.Server/Program.cs
@@ -67,13 +67,13 @@ builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(opti
 builder.Services.AddRemoteBackend(backendApiConfig);
 builder.Services.AddLoginModule();
 
-//builder.Services.UseElsaIdentity();
-builder.Services.UseOAuth2(options =>
-{
-    options.ClientId = "ElsaStudio";
-    options.TokenEndpoint = "https://localhost:44335/connect/token";
-    options.Scope = "AbpSolution1 offline_access";
-});
+builder.Services.UseElsaIdentity();
+//builder.Services.UseOAuth2(options =>
+//{
+//    options.ClientId = "ElsaStudio";
+//    options.TokenEndpoint = "https://localhost:44335/connect/token";
+//    options.Scope = "AbpSolution1 offline_access";
+//});
 
 builder.Services.AddDashboardModule();
 builder.Services.AddWorkflowsModule();
