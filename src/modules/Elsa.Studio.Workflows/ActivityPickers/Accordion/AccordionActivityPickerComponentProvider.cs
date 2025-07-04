cdbfb56b92d3633b963af132a76fd57342651d12
Matt <knibbsy10@live.com>
2025-07-04 05:57:35 +0100

    Add CategoryDisplayResolver to AccordionActivityPicker (#557)
    Implement a new `CategoryDisplayResolver` in the
    `AccordionActivityPickerComponentProvider` for custom
    category name resolution. Update the `ActivityPicker`
    component to use this resolver for displaying category
    names. Include commented-out code for potential future
    implementations of alternative activity pickers and
    designer themes.

--- src/modules/Elsa.Studio.Workflows/ActivityPickers/Accordion/AccordionActivityPickerComponentProvider.cs
+++ src/modules/Elsa.Studio.Workflows/ActivityPickers/Accordion/AccordionActivityPickerComponentProvider.cs
@@ -8,10 +8,17 @@ namespace Elsa.Studio.Workflows.ActivityPickers.Accordion;
 /// </summary>
 public class AccordionActivityPickerComponentProvider : IActivityPickerComponentProvider
 {
+    /// <summary>
+    /// Resolves the display name for a given category string.
+    /// </summary>
+    /// <remarks>The default behavior is to extract the first segment of the category string.</remarks>
+    public Func<string, string> CategoryDisplayResolver = category => category.Split('/').First().Trim();
+
     /// <inheritdoc />
     public RenderFragment GetActivityPickerComponent() => builder =>
     {
         builder.OpenComponent<ActivityPicker>(0);
+        builder.AddAttribute(1, nameof(ActivityPicker.CategoryDisplayResolver), CategoryDisplayResolver);
         builder.CloseComponent();
     };
 }
