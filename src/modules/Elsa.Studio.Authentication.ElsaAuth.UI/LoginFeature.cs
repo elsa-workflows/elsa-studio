733804bc83b7a3792b4863974c6c84ae2c107cae
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-01-11 19:13:07 +0100

    Remove `LoginState` component and associated `LoginFeature` logic from `ElsaAuth` authentication modules.

--- src/modules/Elsa.Studio.Authentication.ElsaAuth.UI/LoginFeature.cs
+++ src/modules/Elsa.Studio.Authentication.ElsaAuth.UI/LoginFeature.cs
@@ -1,18 +1,8 @@
 using Elsa.Studio.Abstractions;
-using Elsa.Studio.Contracts;
-using Elsa.Studio.Authentication.ElsaAuth.UI.Components;
 
 namespace Elsa.Studio.Authentication.ElsaAuth.UI;
 
 /// <summary>
 /// Adds a login app-bar component.
 /// </summary>
-public class LoginFeature(IAppBarService appBarService) : FeatureBase
-{
-    /// <inheritdoc />
-    public override ValueTask InitializeAsync(CancellationToken cancellationToken = default)
-    {
-        appBarService.AddComponent<LoginState>();
-        return base.InitializeAsync(cancellationToken);
-    }
-}
+public class LoginFeature : FeatureBase;
