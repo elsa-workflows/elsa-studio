6a1449d2f023212e873baba7faa8baba53cb2cac
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-05-10 15:08:12 +0200

    Update RemoteFeature name for server logs feature module in Elsa Studio

--- src/modules/Elsa.Studio.ServerLogs/Feature.cs
+++ src/modules/Elsa.Studio.ServerLogs/Feature.cs
@@ -6,8 +6,8 @@ namespace Elsa.Studio.ServerLogs;
 /// <summary>
 /// Represents the server logs feature module for the Elsa Studio application.
 /// </summary>
-[RemoteFeature("Elsa.ServerLogStreaming")]
+[RemoteFeature("Elsa.ServerLogs.ShellFeatures.ServerLogStreaming")]
 public class Feature : FeatureBase
 {
-    public const string RemoteFeatureName = "Elsa.ServerLogStreaming";
+    public const string RemoteFeatureName = "Elsa.ServerLogs.ShellFeatures.ServerLogStreaming";
 }
